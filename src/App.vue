52acb273370ea3faad4d395d1139b13f994cc459
Botir Akhmedov <95535127+botirpyjs@users.noreply.github.com>
2021-12-28 20:12:35 +0300

    Vuejsda birinchi proyektim

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,102 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <div class="body">
+        <br>
+            <!-- <h1 class="text-center text-warning">Add post</h1> -->
+        <div class="w-75 llk">
+            <form @submit.prevent>
+                
+                <input @input="name = $event.target.value" :value="name" type="text" placeholder="Enter your name..." class="form-control">
+                <input @input="email = $event.target.value" :value="email" type="email" placeholder="Enter your email..." class="form-control my-2">
+                <input @input="post = $event.target.value" :value="post" type="text" placeholder="Enter your post..." class="form-control ">
+                <div class="d-flex flex-row-reverse">
+                <button @click="createPost" class="btn btn-primary mt-2 w-25">Add post</button>
+                </div>
+            </form>
+            <hr class="hrrr">
+        </div>
+        <div class="posts w-75" v-for="posts in comments" v-bind:key="posts">
+            <div><b>Name:</b> {{ posts.name }}</div>
+            <hr class="hrr">
+            <div><b>Email:</b> {{ posts.email }}</div>
+            <hr class="hrr">
+            <div class="w-75"><b>Post:</b> <span class="postd fst-italic">"{{ posts.post }}"</span></div>
+        </div>
+    </div>
 </template>
-
+<script>
+export default {
+    data() {
+        return {
+            comments: [
+                {id: 1, name: 'Botir Akhmedov', email: 'botircoders@gmail.com', post: "Assalomu alaykum hurmatli mehmon. Siz bu joyga o'z postlaringizni joylashingiz mumkin."}
+            ],
+            name: "",
+            email: "",
+            post: ""
+            
+            // count: 0,
+        }
+    },
+        methods: {
+            createPost(){
+                const newpost = {
+                    id: Date.now(),
+                    name: this.name,
+                    email: this.email,
+                    post: this.post
+                }
+                this.comments.push(newpost)
+            }
+        },
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+button,
+input{
+    border-radius: 10px !important;
+    border: 1px solid black !important;
 }
-
-#nav {
-  padding: 30px;
+body{
+    background-image: url(dark.jpg);
+    height: auto;
+    width: 100%;
+    background-attachment: fixed;
+    background-size: cover;
+    background-repeat: repeat;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.hrr{
+    border: 1px dashed black;
+    margin: 0;
+    padding: 0px;
+    background: transparent;
+    margin-top: 7px;
+    margin-bottom: 7px;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.body{
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+}
+.posts{
+    background-color: #fff;
+    margin-bottom: 17px;
+    border: 2px rgb(100, 100, 100);
+    border-style: dotted;
+    box-shadow: rgba(214, 214, 214, 0.452) 3px 3px 10px 2px;
+    border-radius: 0px;
+    border-top-left-radius: 0px;
+    border-top-right-radius: 35px;
+    border-bottom-left-radius: 35px;
+    border-bottom-right-radius: 0px;
+    /* margin-left: 20px; */
+    /* width: 1003px; */
+    padding: 15px;
+}
+.hrrr{
+    border: 1px solid white;
+    background: transparent;
 }
 </style>
---
+++ src/Navbar.vue
@@ -0,0 +1,93 @@
+<template>
+    <div>
+        <!-- <h1>
+            <p>
+                Count: <strong>{{ count }}</strong>
+            </p>
+            <button class="btn btn-success" v-on:click="increment">Increment</button>
+            <button class="btn btn-primary" v-on:click="decrement">Decrement</button>
+        </h1> -->
+        <!-- <div class="comment">
+            <div><b>Name:</b> Botir</div>
+        </div> -->
+        <!-- <center>
+            <h1>Navbar qismi </h1>
+        </center>
+        <header>
+            <div class="logo">
+                <span class="lg">LogoMak</span>
+            </div>
+            <div class="menu">
+                <span class="menu1">Home</span>
+                <span class="menu1">About</span>
+                <span class="menu1">Info</span>
+                <span class="menu1">Contact</span>
+                <span class="menu1">Login</span>
+            </div>
+        </header>
+        <h1>for sikldagi holat</h1> -->
+        <div class="bola" v-for="bola in bolalar">
+            <div>Hurmatli {{ bolalar.name }}, siz {{ bolalar.yoshi }} yoshda {{ bolalar.kasbi }}ni egalladingiz!</div>
+            <div>Hurmatli {{ bolalar.name }}, siz {{ bolalar.yoshi }} yoshda {{ bolalar.kasbi }}ni egalladingiz!</div>
+            <div>Hurmatli {{ bolalar.name }}, siz {{ bolalar.yoshi }} yoshda {{ bolalar.kasbi }}ni egalladingiz!</div>
+            <div>Hurmatli {{ bolalar.name }}, siz {{ bolalar.yoshi }} yoshda {{ bolalar.kasbi }}ni egalladingiz!</div>
+        </div>
+        
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            // count: 0,
+            bolalar: [
+                {name: 'Ibrohim', kasbi: 'Dasturchi', yoshi: 17},
+                {name: 'Bobur', kasbi: 'Nonvoy', yoshi: 20},
+                {name: 'Komil', kasbi: 'Dizayner', yoshi: 22},
+                {name: 'Ilxom', kasbi: 'Hofiz', yoshi: 19}
+            ]
+        }
+    },
+        methods: {
+            increment(){
+                this.count += 1
+            },
+            decrement(){
+                this.count -= 1
+            }
+        },
+    
+}
+</script>
+<style>
+header{
+    display: flex;
+    justify-content: space-around;
+    background-color: #444;
+    padding-top: 20px;
+    padding-bottom: 20px;
+}
+.menu{
+    color: white;
+    font-size: 20px;
+    margin-left: 500px;
+    border-left: coral 1px solid;
+    border-right: coral 1px solid;
+}
+.logo{
+    color: white;
+    font-family: cursive;
+    font-size: 20px;
+}
+.menu1{
+    padding-top: 10px;
+    padding-bottom: 10px;
+    cursor: pointer;
+    padding-left: 15px;
+    padding-right: 15px;
+}
+.menu1:hover{
+    transition: .5s all ease;
+    color: coral;
+}
+</style>
